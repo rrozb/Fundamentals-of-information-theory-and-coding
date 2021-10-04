790ca6b88c80a5cb33e8d4efe9a7d64bdd998950
rrozb <rrozbeiko@gmail.com>
2021-10-04 17:13:44 +0200

    text in czech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from entropy import entropy,relative_entropy, average_len
+from entropy import entropy,relative_entropy, average_len,capacity
 from coding import FannoAlgo,HuffmanAlgo
+
 probabilities = [0.01,0.02,0.03,0.02,0.05,0.37,0.02,0.04,0.01,0.39,0.01,0.03]
 
 #Entropy
@@ -8,17 +9,35 @@ calculated_entropy = entropy(probabilities)
 calculated_relative_entropy = relative_entropy(probabilities)
 #Redundancy
 redundancy = 1 - calculated_relative_entropy
-print(calculated_entropy)
-print(calculated_relative_entropy)
-print(redundancy)
 
+print('1. Entropie: ', "{:.4f}".format(calculated_entropy), 'Relativni Entropie:',"{:.4f}".format(calculated_relative_entropy), 'Redundance:',"{:.4f}".format(redundancy))
+
+print('\n2.Fannuv algoritmus:')
 fanno = FannoAlgo(probabilities)
 fanno.run()
-print(fanno.final_codes)
-print('*'*50)
+avg_len_fanno = average_len(fanno.final_codes)
+print('zpráva   ', 'pravděpodobnost zprávy  ','kód zprávy')
+for i,k in fanno.final_codes.items():
+    print(f'{i}\t  {k[2]}\t\t\t   {k[0]}')
+
+print('Střední délka: ', avg_len_fanno)
+print('\n2.Huffmanuv algoritmus:')
 huff = HuffmanAlgo(probabilities)
 huff.run()
-print(huff.final_codes)
+avg_len_huff = average_len(huff.final_codes)
+print('zpráva   ', 'pravděpodobnost zprávy  ','kód zprávy')
+for i,k in huff.final_codes.items():
+    print(f'{i}\t  {k[2]}\t\t\t   {k[0]}')
+print('Střední délka: ', avg_len_huff)
 
-print(average_len(fanno.final_codes))
-print(average_len(huff.final_codes))
+print('\n3.Plati vztah:')
+print(f'{calculated_entropy} < {avg_len_huff} < {avg_len_fanno}')
+
+print('\n4.Shannonova veta pro p=0.13:')
+proba = 0.13
+capacity_of_channel = capacity(proba)
+print(f'Kapacita C: {"{:.4f}".format(capacity_of_channel)}')
+border = "{:.4f}".format(calculated_entropy / capacity_of_channel)
+print(f'Podle Shannonove věty dostáváme hranice n: {border}')
+print('tzn. podle prezentaci:')
+print('Pro n>= 6 lze přenášet zprávy danneho zdroje dannym kanálem s libovolně malou pravděpodobností chyby pomocí vhodného kódování a dekódování')
